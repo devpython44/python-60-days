4dac3f4d7e90ac5c4ce2a73372bf7381e28e721b
Artem <usoaniartem35@gmail.com>
2026-07-15 22:20:54 +0300

    День 11: добавлены инвентарь, трофеи и продажа предметов

---
+++ day11/main.py
@@ -0,0 +1,246 @@
+import random
+import json
+
+player = {
+    "name": "Artem",
+    "level": 1,
+    "xp": 0,
+    "hp": 100,
+    "gold": 0,
+    "damage": 10,
+    "inventory": []
+}
+
+wolf = {
+    "name": "Волк",
+    "hp": 25,
+    "damage": 18,
+    "gold": 20,
+    "xp": 20
+}
+
+goblin = {
+    "name": "Гоблин",
+    "hp": 15,
+    "damage": 8,
+    "gold": 15,
+    "xp": 15
+}
+
+orc = {
+    "name": "Орк",
+    "hp": 40,
+    "damage": 25,
+    "gold": 40,
+    "xp": 40
+}
+
+boss = {
+    "name": "Вожак волков",
+    "hp": 100,
+    "damage": 30,
+    "gold": 100,
+    "xp": 100
+}
+
+def show_player():
+    print("=== Герой ===")
+    print(f"Имя: {player['name']}")
+    print(f"Уровень: {player['level']}")
+    print(f"Опыт: {player['xp']}")
+    print(f"HP: {player['hp']}")
+    print(f"Золото: {player['gold']}")
+    print(f"Урон: {player['damage']}")
+
+def explore_forest():
+    event = random.randint(1,2)
+    if event == 1:
+        player["gold"] += 10
+        player["xp"] += 20
+        save_player()
+        print("Ты получил 20 опыта!")
+        print("Ты нашёл 10 золота!")
+        check_level_up()
+    else:
+        player["hp"] -= 15
+        print("На тебя напал волк!\nТы потерял 15 HP!")
+        if player["hp"] <= 0:
+            save_player()
+            print("Ты погиб!")
+            
+def check_level_up():
+    while player["xp"] >= 100:
+        player["level"] += 1
+        player["xp"] -= 100
+        player["hp"] += 20
+        player["damage"] += 5
+        
+        print("Новый уровень!")
+        print(f"Теперь твой уровень: {player['level']}")
+    
+    save_player()
+
+def random_fight():
+    enemies = [wolf, goblin, orc]
+    enemy = random.choice(enemies).copy()
+
+    fight_enemy(enemy)
+
+def fight_enemy(enemy):
+    print(f"\nТебе встретился {enemy['name']}!\n")
+    
+    while player["hp"] > 0 and enemy["hp"] > 0:
+        enemy["hp"] -= player["damage"]
+        print(f"Ты ударил {enemy['name']} на {player['damage']} урона!")
+        print(f"HP {enemy['name']}: {enemy['hp']}")
+        if enemy["hp"] <= 0:
+            player["gold"] += enemy["gold"]
+            player["xp"] += enemy["xp"]
+            if enemy["name"] == "Волк":
+                if random.randint(1, 100) <= 50:
+                    player["inventory"].append("Клык волка")
+                    print("Получен предмет: Клык волка!")
+            
+            check_level_up()
+            save_player()
+            
+            print(f"""Ты победил {enemy['name']}!
+            Ты получил {enemy['gold']} золота!
+            Ты получил {enemy['xp']} опыта!""")
+
+            break
+        player["hp"] -= enemy["damage"]
+        save_player()
+        print(f"{enemy['name']} ударил тебя на {enemy['damage']} урона!")
+        print(f"Твой HP: {player['hp']}")
+        
+        
+        if player["hp"] <= 0:
+            print("Ты погиб!")
+            break
+
+def fight_boss():
+    fight_enemy(boss.copy())
+
+def save_player(): 
+    with open("player.json", "w", encoding="utf-8") as file:
+        json.dump(player, file, ensure_ascii=False, indent=4)
+        
+def load_player():
+    global player
+    
+    try:
+        with open("player.json", "r", encoding="utf-8") as file:
+            player = json.load(file)
+            
+            if "inventory" not in player:
+                player["inventory"] = []
+            
+            print("Сохранение загружено!")
+            
+    except FileNotFoundError:
+        print("Сохранение не найдено. Создан новый герой.")
+
+def shop():
+    while True:
+        print("""
+    === Магазин ===    
+        
+    1. Зелье лечения (+20 HP) - 20 золота
+    2. Улучшить урон (+5) - 50 золота
+    3. Продать Клык волка - 10 золота
+    4. Выход
+    """)
+        choice = input("Выбор: ")
+        
+        if choice == "1":
+            if player["gold"] >= 20:
+                player["gold"] -= 20
+                player["hp"] += 20
+                save_player()
+                
+                print("Зелье применено!")
+            else:
+                print("Недостаточно золота!")
+                
+        elif choice == "2":
+            if player["gold"] >= 50:
+                player["gold"] -= 50
+                player["damage"] += 5
+                save_player()
+                
+                print("Урон улучшен!")
+            else:
+                print("Недостаточно золота!")    
+                
+        elif choice == "3":
+            if "Клык волка" in player["inventory"]:
+                player["inventory"].remove("Клык волка")
+                player["gold"] += 10
+                save_player()
+                
+                print("Клык волка продан!")
+            else:
+                print("У тебя нет Клыка волка!")
+        
+        elif choice == "4":
+            break
+
+        else:
+            print("Ввод некоректный!")
+
+def show_inventory():
+    if not player["inventory"]:
+        print("Инвентарь пуст.")
+    else:
+        print("=== Инвентарь ===")
+        for i, item in enumerate(player["inventory"], start=1):
+            print(f"{i}. {item}")
+
+# def show_enemy(enemy):
+#     print(enemy["name"])
+#     print(enemy["hp"])
+#     print(enemy["damage"])
+
+menu = """
+=== RPG ===
+
+1. Показать героя
+2. Исследовать лес
+3. Бой
+4. Магазин
+5. Босс
+6. Инвентарь
+7. Выход
+"""
+
+load_player()
+while True:
+    print(menu)
+    choice = input("Выбор: ")
+    
+    if choice == "1":
+        show_player()
+        
+    elif choice == "2":
+        explore_forest()
+        
+    elif choice == "3":
+        random_fight()
+        
+    elif choice == "4":
+        shop()
+    
+    elif choice == "5":
+        fight_boss()
+    
+    elif choice == "6":
+        show_inventory()
+    
+    elif choice == "7":
+        save_player()
+        print("Пока")
+        break
+
+    else:
+        print("Ввод некоректный!")
